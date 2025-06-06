8e2c96a39106892c6d3dbf04b9bebace5034c924
Aren0229 <djt1637@dlsud.edu.ph>
2025-06-07 02:13:45 +0800

    Road + Menu

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+//Controls for a 2D Top-down Driving Sim
+public class InputManager : MonoBehaviour
+{
+	[Header("Movement Controls")]
+	public KeyCode MoveForward = KeyCode.W;
+	public KeyCode MoveBackward = KeyCode.S;
+	public KeyCode MoveLeft = KeyCode.A;
+	public KeyCode MoveRight = KeyCode.D;
+	public KeyCode Handbrake = KeyCode.Space;
+	public KeyCode GearUp = KeyCode.UpArrow;
+	public KeyCode GearDown = KeyCode.DownArrow;
+	public KeyCode Gear1 = KeyCode.Alpha1;
+	public KeyCode Gear2 = KeyCode.Alpha2;
+	public KeyCode Gear3 = KeyCode.Alpha3;
+	public KeyCode GearR = KeyCode.R;
+
+	public bool IsAccelerating { get; private set; } = false;
+	public bool IsDecelerating { get; private set; } = false;
+	public bool IsHandBraking { get; private set; } = false;
+	public bool IsGearingUp { get; private set; } = false;
+	public bool IsGearingDown { get; private set; } = false;
+	public bool SelectGear1 { get; private set; } = false;
+	public bool SelectGear2 { get; private set; } = false;
+	public bool SelectGear3 { get; private set; } = false;
+	public bool SelectGearR { get; private set; } = false;
+	public float Steering { get; private set; } = 0;
+
+	[Header("Menu Controls")]
+	public KeyCode MenuAccept = KeyCode.Return;
+	public KeyCode MenuCancel = KeyCode.Escape;
+
+	public bool PressedAccept { get; private set; } = false;
+	public bool PressedCancel { get; private set; } = false;
+
+	public void InputListener() {
+
+		#region Movement
+
+		IsAccelerating = Input.GetKey(MoveForward);
+		IsDecelerating = Input.GetKey(MoveBackward);
+		IsHandBraking = Input.GetKey(Handbrake);
+
+		if (Input.GetKey(MoveLeft)) Steering = 1;
+		else if (Input.GetKey(MoveRight)) Steering = -1;
+		else if (Input.GetKeyUp(MoveLeft) || Input.GetKeyUp(MoveRight)) Steering = 0;
+		else Steering = 0;
+
+		#endregion
+
+		#region Gears
+
+		IsGearingUp = Input.GetKey(GearUp);
+		IsGearingDown = Input.GetKeyDown(GearDown);
+
+		SelectGear1 = Input.GetKeyDown(Gear1);
+		SelectGear2 = Input.GetKeyDown(Gear2);
+		SelectGear3 = Input.GetKeyDown(Gear3);
+		SelectGearR = Input.GetKeyDown(GearR);
+
+		#endregion
+
+		#region Menu Controls
+
+		PressedAccept = Input.GetKeyDown(MenuAccept);
+		PressedCancel = Input.GetKeyDown(MenuCancel);
+
+		#endregion
+	}
+
+	private void Update() {
+		InputListener();
+	}
+}
