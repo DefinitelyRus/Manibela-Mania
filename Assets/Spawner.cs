dd48b9bd9c1f3ae49d7aeae2c954c73f7ba9392f
Aren0229 <djt1637@dlsud.edu.ph>
2025-06-07 17:48:47 +0800

    Road Generation

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 
 public class VerticalWorldSpawner : MonoBehaviour
 {
-    public GameObject[] worldPrefabs;         // World1 to World6
-    public Transform player;                  // Reference to the player
-    public float spawnDistanceAhead = 100f;   // Distance ahead of the player to spawn new worlds
-    public float despawnDistance = 60f;       // Distance below player to despawn old worlds
-    public float worldHeight = 50f;           // Vertical size of each world prefab
+    public GameObject[] worldPrefabs;         // 0-2 main, 3-5 transition
+    public Transform player;
+    public float spawnDistanceAhead = 100f;
+    public float despawnDistance = 60f;
+    public float worldHeight = 50f;
 
-    private float nextSpawnY = 0f;            // Next Y coordinate to spawn at
+    private float nextSpawnY = 0f;
     private List<GameObject> spawnedWorlds = new List<GameObject>();
+    private int lastPrefabIndex = -1;
+    private int forcedPrefab2Count = 0;  // NEW: Counter for forced spawns of element 2
 
     void Start()
     {
-        // Initial spawn
         for (int i = 0; i < 3; i++)
         {
             SpawnWorld();
@@ -23,13 +24,11 @@ public class VerticalWorldSpawner : MonoBehaviour
 
     void Update()
     {
-        // Spawn new worlds ahead of player
         while (player.position.y + spawnDistanceAhead > nextSpawnY)
         {
             SpawnWorld();
         }
 
-        // Despawn worlds behind the player
         for (int i = spawnedWorlds.Count - 1; i >= 0; i--)
         {
             if (player.position.y - spawnedWorlds[i].transform.position.y > despawnDistance)
@@ -42,11 +41,45 @@ public class VerticalWorldSpawner : MonoBehaviour
 
     void SpawnWorld()
     {
-        GameObject prefab = worldPrefabs[Random.Range(0, worldPrefabs.Length)];
+        int prefabIndex;
+
+        // Force element 2 for first two spawns
+        if (forcedPrefab2Count < 2)
+        {
+            prefabIndex = 2;
+            forcedPrefab2Count++;
+        }
+        else if (lastPrefabIndex == -1)
+        {
+            // After forced spawns, start with standard logic
+            prefabIndex = Random.Range(0, 3);
+        }
+        else if (lastPrefabIndex == 0)
+        {
+            prefabIndex = Random.Range(3, 6);
+        }
+        else if (lastPrefabIndex == 1 || lastPrefabIndex == 2)
+        {
+            prefabIndex = Random.Range(0, 3);
+        }
+        else if (lastPrefabIndex == 3)
+        {
+            prefabIndex = Random.Range(0, 3);
+        }
+        else if (lastPrefabIndex == 4 || lastPrefabIndex == 5)
+        {
+            prefabIndex = Random.Range(3, 6);
+        }
+        else
+        {
+            prefabIndex = Random.Range(0, worldPrefabs.Length);
+        }
+
         Vector3 spawnPos = new Vector3(0f, nextSpawnY, 0f);
-        GameObject newWorld = Instantiate(prefab, spawnPos, Quaternion.identity);
+        GameObject newWorld = Instantiate(worldPrefabs[prefabIndex], spawnPos, Quaternion.identity);
         spawnedWorlds.Add(newWorld);
 
+        lastPrefabIndex = prefabIndex;
         nextSpawnY += worldHeight;
     }
 }
