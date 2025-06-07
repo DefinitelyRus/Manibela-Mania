df8c32915cc6380aea36cfc20934ff5ba5c5df97
Aren0229 <djt1637@dlsud.edu.ph>
2025-06-07 22:01:38 +0800

    Scaled

---
+++ Assets/Scripts/GlobalCounter.cs
@@ -0,0 +1,8 @@
+// GlobalCounter.cs
+public static class GlobalCounter
+{
+    public static int Char1 = 0;
+    public static int Char2 = 0;
+    public static int Char3 = 0;
+    public static int Char4 = 0;
+}
