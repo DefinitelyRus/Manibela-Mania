b55e85f65a7dd3d9d8c19e8161b449a2f5a5177e
Rus <definitelyrus@gmail.com>
2025-06-10 22:33:19 +0800

    Repushed everything.

--- Assets/Scripts/PassengerData.cs
+++
@@ -1,8 +0,0 @@
-// PassengerData.cs
-public class PassengerData
-{
-    public string passengerName;
-    public UnityEngine.GameObject passengerPrefab;
-    public int dropoffCountdown;
-    public int dropoffPrefabIndex;
-}
